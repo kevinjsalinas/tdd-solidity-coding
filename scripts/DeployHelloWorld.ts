f0a07417e29b9d10e81055136fd9b3e5b1611255
Kevin Salinas <kevinsalinas155@gmail.com>
2023-09-15 15:35:22 -0400

    pull info from ethers in script

--- scripts/DeployHelloWorld.ts
+++ scripts/DeployHelloWorld.ts
@@ -1,7 +1,33 @@
-function main() {
+import { ethers } from 'hardhat';
+import { Bytecode } from 'hardhat/internal/hardhat-network/stack-traces/model';
 
-    console.log("It's working");
+async function main() {
+
+    const accounts = await ethers.getSigners();
+    const [deployer, acc1, acc2] = accounts;
+    console.log(`The address of the deployer is ${deployer.address}`);
+    const provider = ethers.provider;
+    const lastBlock = await provider.getBlock("latest");
+    console.log("The last block in this network is: \n");
+    console.log({lastBlock});
+
+    const deployerBalance = await provider.getBalance(deployer.address);
+    console.log(`The balance of the deployer is ${ethers.formatUnits(deployerBalance)} ETH`);
+
+    const helloWorldFactory = await ethers.getContractFactory("HelloWorld");
+    const helloWorldContract = await helloWorldFactory.deploy();
+    await helloWorldContract.waitForDeployment();
+
+    const helloWorldContractAddress = await helloWorldContract.getAddress();
+    console.log(`The address of the smart contract is ${helloWorldContractAddress}`)
+    
+    const helloWorldBytes = await helloWorldContract.helloWorld();
+    const helloWorld = ethers.decodeBytes32String(helloWorldBytes);
+    console.log(`The text stored in the contract is ${helloWorld}`);
 
 }
 
-main();
+main().catch((err) => {
+    console.error(err);
+    process.exitCode = 1;
+});
